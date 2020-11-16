a03c33974b417254155e58421833bd32b43d8c8a
hengsoo <hengsoo.sjh@gmail.com>
2020-11-16 23:05:12 +0800

    feat: make tag writable in creating properties

--- thu_lost_and_found_backend/property_service/serializer.py
+++ thu_lost_and_found_backend/property_service/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from thu_lost_and_found_backend.property_service.models import PropertyType, PropertyTemplate, Property
 from thu_lost_and_found_backend.tag_service.models import Tag
+from thu_lost_and_found_backend.tag_service.serializer import TagSimpleSerializer
 
 
 class PropertyTemplateSerializer(serializers.ModelSerializer):
@@ -24,10 +25,19 @@ class PropertyTypeSerializer(serializers.ModelSerializer):
 class PropertySerializer(serializers.ModelSerializer):
     template_queryset = PropertyTemplate.objects.all()
     template = serializers.PrimaryKeyRelatedField(queryset=template_queryset)
-    tag_queryset = Tag.objects.all()
-    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=tag_queryset)
+    tags = TagSimpleSerializer(many=True)
 
-    # TODO: add notices
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        _property = Property.objects.create(**validated_data)
+        for tag_data in tags_data:
+            tag, created = Tag.objects.get_or_create(**tag_data)
+            _property.tags.add(tag)
+        _property.save()
+        return _property
+
+    # TODO: update
+
     class Meta:
         model = Property
         fields = '__all__'
--- thu_lost_and_found_backend/tag_service/serializer.py
+++ thu_lost_and_found_backend/tag_service/serializer.py
@@ -4,7 +4,12 @@ from thu_lost_and_found_backend.tag_service.models import Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Tag
         fields = '__all__'
+
+
+class TagSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name', 'color']
