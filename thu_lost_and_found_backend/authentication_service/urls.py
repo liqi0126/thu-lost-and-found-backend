a259c1aaa41a48256982d9c1d2baa9844056bea2
hengsoo <hengsoo.sjh@gmail.com>
2020-11-28 21:00:33 +0800

    Revert "feat: add wechat authencitaction (NOT TESTED)"
    This reverts commit 23833909d9459fd7dd0325582b1cf90a88c8da7e.

--- thu_lost_and_found_backend/authentication_service/urls.py
+++ thu_lost_and_found_backend/authentication_service/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('wechat/token/', views.wechat_token, name='wechat_token_obtain_pair'),
-    path('wechat/component-verify-ticket/', views.wechat_component_verify_ticket,
-         name='wechat_component_verify_ticket'),
+    path('wechat/token/refresh/', views.wechat_token_refresh, name='wechat_token_refresh'),
 ]
