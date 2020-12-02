13b09e7828af99c1db2f6af3d73b3a22945bc111
hengsoo <hengsoo.sjh@gmail.com>
2020-12-02 22:39:58 +0800

    feat: send verification email

--- thu_lost_and_found_backend/user_service/views.py
+++ thu_lost_and_found_backend/user_service/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import datetime, timedelta
 
 from django.contrib.auth.hashers import make_password
@@ -12,19 +13,20 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from thu_lost_and_found_backend import settings
-from thu_lost_and_found_backend.helpers.toolkits import delete_instance_medias, check_missing_fields
+from thu_lost_and_found_backend.helpers.toolkits import delete_instance_medias, check_missing_fields, random_string
 from thu_lost_and_found_backend.user_service.models import User, UserVerificationApplication, UserInvitation, \
     UserEmailVerification
 from thu_lost_and_found_backend.user_service.serializer import UserSerializer, UserVerificationApplicationSerializer, \
     UserInvitationSerializer, UserEmailVerificationSerializer
+from .email_verification_template import email_verification_template
 from .invitation_template import invitation_template
-from ..authentication_service.permission import SuperAdminOnlyPermission, UserPermission
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-#    permission_classes = [UserPermission]
+
+    #    permission_classes = [UserPermission]
 
     def perform_destroy(self, instance):
         delete_instance_medias(instance, 'avatar')
@@ -39,19 +41,19 @@ class UserViewSet(viewsets.ModelViewSet):
 class UserVerificationApplicationViewSet(viewsets.ModelViewSet):
     queryset = UserVerificationApplication.objects.all()
     serializer_class = UserVerificationApplicationSerializer
+    #    permission_classes = [UserPermission]
     # TODO: implement this
 
 
 class UserInvitationViewSet(viewsets.ModelViewSet):
     queryset = UserInvitation.objects.all()
     serializer_class = UserInvitationSerializer
-#    permission_classes = [SuperAdminOnlyPermission]
+
+    #    permission_classes = [SuperAdminOnlyPermission]
 
     def create(self, request, *args, **kwargs):
-
-        request.data['token'] = \
-            User.objects.make_random_password(length=64,
-                                              allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
+        request.POST._mutable = True
+        request.data['token'] = random_string(64)
         if 'expiration_date' not in request.data:
             request.data['expiration_date'] = datetime.now() + timedelta(weeks=2)
 
@@ -72,7 +74,7 @@ class UserInvitationViewSet(viewsets.ModelViewSet):
                   html_message=invitation_template.format(
                       role=request.data['role'],
                       invitation_link=f'{settings.APP_URL}/#/invitation/f{request.data["token"]}/',
-                      expiration_date=request.data['expiration_date'].strftime('%m-%d-%Y')
+                      expiration_date=request.data['expiration_date'].strftime('%d-%m-%Y')
                   ),
 
                   from_email=f'"{settings.EMAIL_DISPLAY_NAME}" <{settings.EMAIL_HOST_USER}>',
@@ -130,4 +132,38 @@ class UserInvitationViewSet(viewsets.ModelViewSet):
 class UserEmailVerificationViewSet(viewsets.ModelViewSet):
     queryset = UserEmailVerification.objects.all()
     serializer_class = UserEmailVerificationSerializer
-    # TODO: implement this
+
+    def create(self, request, *args, **kwargs):
+
+        request.POST._mutable = True
+        request.data['token'] = random_string(64)
+        if 'expiration_date' not in request.data:
+            request.data['expiration_date'] = datetime.now() + timedelta(weeks=2)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Check if user is already verified
+        user = User.objects.get(pk=request.data['user'])
+
+        if user.is_verified:
+            return HttpResponseBadRequest(f'User "{user.username}" has already been verified.')
+        # Check if valid email
+        if not re.match(string=request.data['email'], pattern=r'.+@mails?\.tsinghua\.edu\.cn$'):
+            return HttpResponseBadRequest('Email must be Tsinghua\'s email.')
+
+        self.perform_create(serializer)
+
+        send_mail(subject='THU Lost-and-Found Email Verification Link',
+                  message='',
+                  html_message=email_verification_template.format(
+                      username=user.username,
+                      verification_link=f'{settings.APP_URL}/#/email-verification/f{request.data["token"]}/',
+                      expiration_date=request.data['expiration_date'].strftime('%d-%m-%Y')
+                  ),
+                  from_email=f'"{settings.EMAIL_DISPLAY_NAME}" <{settings.EMAIL_HOST_USER}>',
+                  recipient_list=[request.data['email']],
+                  fail_silently=False)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
