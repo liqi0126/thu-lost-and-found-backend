c06c9334107905babbc7e61472d4f7a8a383b571
hengsoo <hengsoo.sjh@gmail.com>
2020-12-08 00:47:26 +0800

    fix: remove duplicated filter settings

--- thu_lost_and_found_backend/settings.py
+++ thu_lost_and_found_backend/settings.py
@@ -172,7 +172,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
-        'rest_framework.filters.OrderingFilter',
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
