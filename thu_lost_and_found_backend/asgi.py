aa0af5952972a8a5efdd4ff76e39655fdfa9ff74
Qi Li <liqi17thu@gmail.com>
2020-12-14 18:13:20 +0800

    debug: asgi setting

--- thu_lost_and_found_backend/asgi.py
+++ thu_lost_and_found_backend/asgi.py
@@ -6,18 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thu_lost_and_found_backend.settings')
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
-import os
-import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from thu_lost_and_found_backend.chat_service import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thu_lost_and_found_backend.settings')
-django.setup()
-
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
