cfade8a40cb6a5fe938fb3487ca37895032e0a3c
Qi Li <liqi17thu@gmail.com>
2020-12-20 12:57:18 +0800

    chore: matching serializer images from CHAR to JSON

--- thu_lost_and_found_backend/matching_service/serializer.py
+++ thu_lost_and_found_backend/matching_service/serializer.py
@@ -4,10 +4,10 @@ from .models import MatchingEntry
 
 
 class MatchingEntrySerializer(serializers.ModelSerializer):
-    lost_notice_images = serializers.CharField(source='lost_notice.images')
+    lost_notice_images = serializers.JSONField(source='lost_notice.images')
     lost_notice_description = serializers.CharField(source='lost_notice.description')
     lost_property_name = serializers.CharField(source='lost_notice.property.name')
-    found_notice_images = serializers.CharField(source='found_notice.images')
+    found_notice_images = serializers.JSONField(source='found_notice.images')
     found_notice_description = serializers.CharField(source='found_notice.description')
     found_property_name = serializers.CharField(source='found_notice.property.name')
 
