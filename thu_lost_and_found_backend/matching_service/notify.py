aed88d317cc9f93f76d904eb3dda4ddc6d692033
Qi Li <liqi17thu@gmail.com>
2020-12-21 22:52:26 +0800

    fix: single var for admin consumer

--- thu_lost_and_found_backend/matching_service/notify.py
+++ thu_lost_and_found_backend/matching_service/notify.py
@@ -4,21 +4,19 @@ from django.core.mail import send_mail
 
 from thu_lost_and_found_backend import settings
 from thu_lost_and_found_backend.matching_service.models import MatchingEntry
-from thu_lost_and_found_backend.found_notice_service.models import FoundNotice
-from thu_lost_and_found_backend.lost_notice_service.models import LostNotice
 from thu_lost_and_found_backend.chat_service.consumers import ChatConsumer
 
 from .email_matched_notify_template import email_matched_notify_template
 
+admin_consumer = ChatConsumer()
+
 
 def matching_notify(matching_entry: MatchingEntry, just_sent=False):
-
     lost_notice = matching_entry.lost_notice
     found_notice = matching_entry.found_notice
     matching_degree = matching_entry.matching_degree
 
     # message in wechat mini program
-    admin_consumer = ChatConsumer()
     admin_consumer.send_message(1, lost_notice.author.id, json.dumps({
         "lost_notice": lost_notice.id,
         "found_notice": found_notice.id,
