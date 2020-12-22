0c9eab434131c77637e02b15b643106fb0013faf
Qi Li <liqi17thu@gmail.com>
2020-12-23 01:20:08 +0800

    fix: Message CASCADE

--- thu_lost_and_found_backend/chat_service/models.py
+++ thu_lost_and_found_backend/chat_service/models.py
@@ -4,8 +4,8 @@ from thu_lost_and_found_backend.user_service.models import User
 
 # Create your models here.
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='received_messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     message = models.TextField()
     sent = models.BooleanField(default=False)
     time = models.DateTimeField(auto_now_add=True, blank=True)
