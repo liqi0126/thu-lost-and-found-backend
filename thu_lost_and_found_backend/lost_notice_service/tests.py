bc806bd3424788d8da78a801890ac8f4c800a6a6
hengsoo <hengsoo.sjh@gmail.com>
2020-12-23 22:26:21 +0800

    test: add client

--- thu_lost_and_found_backend/lost_notice_service/tests.py
+++ thu_lost_and_found_backend/lost_notice_service/tests.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
-from django.test import TestCase
-
+from django.test import TestCase, Client
 from thu_lost_and_found_backend.property_service.models import PropertyType, PropertyTemplate
 from thu_lost_and_found_backend.user_service.models import User
 
 
 class LostNoticeTestCase(TestCase):
     def setUp(self):
+
+        client = Client()
+        
         user = User.objects.create(username='john', password='secret', first_name='Thu', last_name='Student',
                                    is_verified=True, status='ACT', is_staff=False, is_superuser=False,
                                    date_joined=datetime.now())
