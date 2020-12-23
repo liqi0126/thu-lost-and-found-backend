fab8b2b0050dacf93667019b7b31adeb4d56437d
Qi Li <liqi17thu@gmail.com>
2020-12-24 00:53:48 +0800

    feat: allow CORS *

--- thu_lost_and_found_backend/settings.py
+++ thu_lost_and_found_backend/settings.py
@@ -30,6 +30,10 @@ APP_URL = env('APP_URL')
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    "*"
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
 
+    'corsheaders',
     'channels',
     'rest_framework',
 
@@ -63,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
